c738dddcf6d8b4b7abdf078b3244dd1ab3431ee9
zmh0531 <zhaominghui5@huawei.com>
2025-07-30 20:22:38 +0800

    fix:update repo

--- tests/llm/test_llm.py
+++
@@ -1,8 +0,0 @@
-from langchain_core.messages import HumanMessage
-from src.llm.llm_wrapper import LLMWrapper
-
-if __name__ == "__main__":
-    client = LLMWrapper("basic")
-    msgs = [HumanMessage(content="Hello")]
-    resp = client.invoke(msgs)
-    print(resp)
