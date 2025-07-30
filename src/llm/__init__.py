aa019e05e5651ece7cea507b7b322d6a4d71de8b
zmh0531 <zhaominghui5@huawei.com>
2025-07-30 20:18:06 +0800

    fix:update repo

--- src/llm/__init__.py
+++
@@ -1,6 +0,0 @@
-from .llm_wrapper import LLMWrapper
-from .openai_creator import OpenAICreator
-from .deepseek_creator import DeepSeekCreator
-
-LLMWrapper.register("openai", OpenAICreator)
-LLMWrapper.register("deepseek", DeepSeekCreator)
